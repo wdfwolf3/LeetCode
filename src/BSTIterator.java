f01d861e58489b5ecfe26bf0d611b6c0eb22c759
wdfwolf3 <fwolff@vip.qq.com>
2017-05-15 18:23:34 +0800

    同步

---
+++ src/BSTIterator.java
@@ -0,0 +1,45 @@
+import java.util.Stack;
+
+public class BSTIterator {
+	private Stack<TreeNode> stack = new Stack<>();
+	TreeNode current;
+	
+	public BSTIterator(TreeNode root) {
+		if(root != null)
+			pushLeftTreeNode(root);	
+	}
+
+	/** @return whether we have a next smallest number */
+	public boolean hasNext() {
+		if(stack.isEmpty())
+			return false;
+		current = stack.pop();		
+		if(current.right != null){
+			pushLeftTreeNode(current.right);	
+		}
+		return true;
+	}
+
+	/** @return the next smallest number */
+	public int next() {
+		return current.val;
+	}
+	
+	private void pushLeftTreeNode(TreeNode treeNode){
+		stack.push(treeNode);
+		while (treeNode.left!=null){
+			treeNode = treeNode.left;
+			stack.push(treeNode);
+		}	
+	}
+}
+
+class TreeNode {
+	int val;
+	TreeNode left;
+	TreeNode right;
+
+	TreeNode(int x) {
+		val = x;
+	}
+}
---
+++ src/LongestWordinDictionarythroughDeleting.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by wdfwolf3 on 2017/5/15.
+ */
+public class LongestWordinDictionarythroughDeleting {
+    public String findLongestWord(String s, List<String> d) {
+        List<String> ans = new ArrayList<>();
+        int maxLength = 0;
+        for(String string : d){
+            int i=0;
+            for(int j=0;i<string.length();i++,j++){
+                char c = string.charAt(i);
+                while(j<s.length() && c!=s.charAt(j))
+                    j++;
+                if(j == s.length())
+                    break;
+            }
+            if(i == string.length()){
+                if(i > maxLength){
+                    ans = new ArrayList<>();
+                    ans.add(string);
+                    maxLength = i;
+                }else if(i == maxLength)
+                    ans.add(string);
+            }
+        }
+        if(ans.size() == 0)
+            return "";
+        System.out.println(ans.size());
+        Collections.sort(ans);
+        return ans.get(0);
+    }
+}
