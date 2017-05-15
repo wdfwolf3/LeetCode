fc49df202ff54a2cc17e6b4ba6085b40a29d6015
wdfwolf3 <fwolff@vip.qq.com>
2017-05-15 23:15:16 +0800

    同步

--- src/DecodeString.java
+++ src/DecodeString.java
@@ -4,38 +4,36 @@ import java.util.Stack;
  * Created by wdfwolf3 on 2017/5/13.
  */
 public class DecodeString {
+    public static void main(String[] args) {
+        System.out.println((new DecodeString()).decodeString("100[leetcode]"));
+    }
+
     public String decodeString(String s) {
-        String res = "";
-        Stack<Integer> countStack = new Stack<>();
-        Stack<String> resStack = new Stack<>();
-        int idx = 0;
-        while (idx < s.length()) {
-            if (Character.isDigit(s.charAt(idx))) {
-                int count = 0;
-                while (Character.isDigit(s.charAt(idx))) {
-                    count = 10 * count + (s.charAt(idx) - '0');
-                    idx++;
+        String current = "";
+        Stack<Integer> numbers = new Stack<>();
+        Stack<String> stringStack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if(Character.isDigit(c)){
+                int j = i+1;
+                while (Character.isDigit(s.charAt(j)))
+                    j++;
+                numbers.push(Integer.parseInt(s.substring(i, j)));
+                i = j - 1;
+            }else if (Character.isAlphabetic(c)){
+                current += c;
+            }else if(c == '['){
+                stringStack.push(current);
+                current = "";
+            }else {
+                int count = numbers.pop();
+                StringBuilder tmp = new StringBuilder(current);
+                for (int j = 1; j < count; j++) {
+                    tmp.append(current);
                 }
-                countStack.push(count);
-            }
-            else if (s.charAt(idx) == '[') {
-                resStack.push(res);
-                res = "";
-                idx++;
-            }
-            else if (s.charAt(idx) == ']') {
-                StringBuilder temp = new StringBuilder (resStack.pop());
-                int repeatTimes = countStack.pop();
-                for (int i = 0; i < repeatTimes; i++) {
-                    temp.append(res);
-                }
-                res = temp.toString();
-                idx++;
-            }
-            else {
-                res += s.charAt(idx++);
+                current = stringStack.pop() + tmp.toString();
             }
         }
-        return res;
+        return current;
     }
 }
